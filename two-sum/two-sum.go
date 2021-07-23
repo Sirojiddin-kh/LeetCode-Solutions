b0b7707f171c24c1cae1d1e72939c55eb3713384
Sirojiddin Khomidov <69724810+Sirojiddin-Kh@users.noreply.github.com>
2021-07-23 12:23:00 +0500

    Time: 48 ms (12.01%), Space: 3.8 MB (87.77%) - LeetHub

---
+++ two-sum/two-sum.go
@@ -0,0 +1,13 @@
+func twoSum(nums []int, target int) []int {
+    
+    var result []int
+    
+    for i := 0; i < len(nums); i++ {
+        for j := i +1; j < len(nums); j++ {
+            if nums[i] + nums[j] == target {
+                result = append(result, i, j)
+            }
+        }
+    }
+    return result
+}
